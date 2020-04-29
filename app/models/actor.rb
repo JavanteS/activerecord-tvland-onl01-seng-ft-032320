65d2dc96b49bf9bcf0e7691a40df8464b6059f00
Javante Nelson <javantenelson@gmail.com>
2020-04-29 12:53:46 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,8 @@ class Actor < ActiveRecord::Base
   has_many :characters 
   has_many :shows, through: :characters
   
-  def full_name 
+  def full_name
+    binding.pry 
   end 
   
   def list_roles 
