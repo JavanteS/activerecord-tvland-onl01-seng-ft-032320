6e39c98deeba4a253c48e516c2fa62d7eba32a7f
Javante Nelson <javantenelson@gmail.com>
2020-04-29 12:09:34 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -5,8 +5,8 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t| 
       t.string :name 
-      t.string 
-      t.string 
+      t.integer :actor_id  
+      t.integer :show_id  
     end 
   end
 end
